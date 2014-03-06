c7e95097944899e7f3bb74c604dc79ca06dae3fa
AdamPannell <apannel1@email.cpcc.edu>
2014-03-06 12:33:41 -0500

    Login Slider Clean up
    Login Slider Clean up

--- js/Login_Slider.js
+++ js/Login_Slider.js
@@ -2,14 +2,14 @@
 //Login Slider Script
 
 $(document).ready(function() {
-  $('#openLoginForm').toggle(
-      function() {
-	     $('#login form').slideDown(300);
-	     $(this).addClass('close');
-	  },
-	  function() {
-		 $('#login form').slideUp(300);
-	     $(this).removeClass('close');
-      }
-  ); // end toggle
+	$('#openLoginForm').toggle(
+		function() {
+			$('#login form').slideDown(300);
+			$(this).addClass('close');
+		},
+		function() {
+			$('#login form').slideUp(300);
+			$(this).removeClass('close');
+		}
+	); // end toggle
 }); // end ready
